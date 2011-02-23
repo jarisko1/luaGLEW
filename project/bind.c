72eea2186219095aa4e6bad0f5436eb15de43196
anubid34 <jaroslav.svarc.mail@gmail.com>
2011-02-23 18:24:09 +0100

    new version 2.20

--- project/bind.c
+++ project/bind.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "lua.h"
 #include "lauxlib.h"
@@ -1403,7 +1404,9 @@ int luaglew_const_bind(lua_State *L)
 //////////////////////////////////////////////////////////////////////////
 //---------------------- FUNCTIONS DEFINITIONS -------------------------//
 
-static int luaglew_enum(lua_State *L, int index)
+/////////////////////// MINE FUNCTIONS //////////////////////////
+
+static int luaglew_checkenum(lua_State *L, int index)
 {
     int i, value=0;
     const char *enumer=luaL_checkstring(L,index);
@@ -1422,6 +1425,226 @@ static int luaglew_enum(lua_State *L, int index)
     return value;
 }
 
+//////////////////////////// CHECK FIELDS 2D (char, void) ///////////////////////////////////
+// 2D VOID  // ako reprezentovany void??
+static void **luaglew_checkarray_2void(lua_State *L, int index)
+{
+    int n1, n2, i, j;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n1 = luaL_getn(L, index);
+
+    void **pole = (void **) malloc(n1 * sizeof(void *));
+
+    for (i = 0; i < n1; i++) {
+        lua_rawgeti(L, index, i+1);
+
+        luaL_checktype(L, index+1, LUA_TTABLE);
+        n2 = luaL_getn(L, index+1);
+        pole[i]=(void *) malloc(n2 * sizeof(void));
+        for(j = 0; j < n2; j++)
+        {
+            lua_rawgeti(L, index+1, j+1);
+            //pole[i][j]=(void)luaL_checkany(L, -1);
+            lua_remove(L, -1);
+        }
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+// 2D CHAR = 1D STRING  // bere jak string aj cisla. neni to chyba?
+static char **luaglew_checkarray_2char(lua_State *L, int index)
+{
+    int n, i;
+    char str[300];
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n = luaL_getn(L, index);
+
+    char **pole = (char **) malloc(n*sizeof(char *));
+
+    for (i = 0; i < n; i++) {
+        lua_rawgeti(L, index, i+1);
+        strcpy(str,luaL_checkstring(L, -1));
+        pole[i]=(char *) malloc((strlen(str))*sizeof(char *));
+        strcpy(pole[i],str);
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+//////////////////////////// CHECK FIELDS 1D (boolean, byte, ubyte, char, double, enum, float, int, unit, short, ushort, long(sizei), ulong)///////////////////////////////////
+// 1D BOOLEAN  // je spravne reprezentovat bool ako int?
+static int *luaglew_checkarray_boolean(lua_State *L, int index)
+{
+    int n, i;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n = luaL_getn(L, index);
+
+    int *pole = (int *) malloc(n*sizeof(int));
+
+    for (i = 0; i < n; i++) {
+        lua_rawgeti(L, index, i+1);
+        pole[i]=lua_toboolean(L, -1);
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+// 1D BYTE
+// 1D UBYTE
+// 1D CHAR = 1 STRING
+static char *luaglew_checkarray_char(lua_State *L, int index)
+{
+    char str[300];
+    char *pole;
+
+    strcpy(str,luaL_checkstring(L, index));
+    pole=(char *) malloc((strlen(str))*sizeof(char *));
+    strcpy(pole,str);
+    lua_remove(L, -1);
+
+    return pole;
+}
+// 1D DOUBLE
+static double *luaglew_checkarray_double(lua_State *L, int index)
+{
+    int n, i;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n = luaL_getn(L, index);
+
+    double *pole = (double *) malloc(n*sizeof(double));
+
+    for (i = 0; i < n; i++) {
+        lua_rawgeti(L, index, i+1);
+        pole[i]=(double)luaL_checknumber(L, -1);
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+// 1D ENUM
+static int *luaglew_checkarray_enum(lua_State *L, int index)
+{
+    int n, i;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n = luaL_getn(L, index);
+
+   int *pole = (int *) malloc(n*sizeof(int));
+
+    for (i = 0; i < n; i++) {
+        lua_rawgeti(L, index, i+1);
+        pole[i]=luaglew_checkenum(L, -1);
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+//1D FLOAT
+static float *luaglew_checkarray_float(lua_State *L, int index)
+{
+    int n, i;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n = luaL_getn(L, index);
+
+   float *pole = (float *) malloc(n*sizeof(float));
+
+    for (i = 0; i < n; i++) {
+        lua_rawgeti(L, index, i+1);
+        pole[i]=(float)luaL_checknumber(L, -1);
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+// 1D INT
+static int *luaglew_checkarray_int(lua_State *L, int index)
+{
+    int n, i;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n = luaL_getn(L, index);
+
+    int *pole = (int *) malloc(n*sizeof(int));
+
+    for (i = 0; i < n; i++) {
+        lua_rawgeti(L, index, i+1);
+        pole[i]=luaL_checkint(L, -1);
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+
+/*
+//ziska 1-rozmerne pole typu INT zo zasobnika
+static int *luaglew_checkarray_int(lua_State *L, int index)
+{
+    int n, i;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n = luaL_getn(L, index);
+
+    int *pole = (int *) malloc(n*sizeof(int));
+
+    for (i = 0; i < n; i++) {
+        lua_rawgeti(L, index, i+1);
+        pole[i]=luaL_checkint(L, -1);
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+
+//ziska 2-rozmerne pole typu INT zo zasobnika
+static int **luaglew_checkarray_2int(lua_State *L, int index)
+{
+    int n1, n2, i, j;
+
+    luaL_checktype(L, index, LUA_TTABLE);
+    n1 = luaL_getn(L, index);
+
+    int **pole = (int **) malloc(n1 * sizeof(int *));
+
+    for (i = 0; i < n1; i++) {
+        lua_rawgeti(L, index, i+1);
+
+        luaL_checktype(L, index+1, LUA_TTABLE);
+        n2 = luaL_getn(L, index+1);
+        pole[i]=(int *) malloc(n2 * sizeof(int));
+        for(j = 0; j < n2; j++)
+        {
+            lua_rawgeti(L, index+1, j+1);
+            pole[i][j]=luaL_checkint(L, -1);
+            lua_remove(L, -1);
+        }
+        lua_remove(L, -1);
+    }
+    lua_remove(L, -1);
+    return pole;
+}
+*/
+/* testovanie ziskavania tabulky do poli */
+static int luaglew_test_function(lua_State *L)
+{
+    char **pole;
+    int i;
+    pole = luaglew_checkarray_2char(L, 1);
+    for(i=0;i<3;i++)
+        printf("%s ",pole[i]);
+    printf("\n");
+    return 0;
+}
+
+
+
+
+/////////////////////// OPENGL FUNCTIONS //////////////////////////
 
 // void glViewport(GLint x, GLint y, GLsizei width, GLsizei height)
 static int luaglew_glViewport(lua_State *L)
@@ -1454,7 +1677,7 @@ static int luaglew_glClear(lua_State *L)
 //void glBegin(	GLenum  	mode)
 static int luaglew_glBegin(lua_State *L)
 {
-    glBegin(luaglew_enum(L,1));
+    glBegin(luaglew_checkenum(L,1));
     return 0;
 }
 
@@ -1505,6 +1728,7 @@ static const struct luaL_Reg luaglew_lib[] = {
     {"glEnd",luaglew_glEnd},
     {"glShadeModel",luaglew_glShadeModel},
     {"glClearColor",luaglew_glClearColor},
+    {"luaglew_test_function",luaglew_test_function},
     {NULL,NULL}
 };
 
